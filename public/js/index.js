079623ce2240e469b87f860a5a20bc77ecce5744
Daniel Pelstring <danielpelstring@macbookpro.lan>
2025-02-20 14:01:48 -0500

    tried to hide API key but don't know if it worked and now the random images are no longer showing

--- public/js/index.js
+++ public/js/index.js
@@ -10,12 +10,11 @@ const el = {
 //https://api.unsplash.com/photos/!client_id=yowF489jYmvy_0eVWJW5ClELc7mYi_LNJKLjNN3A2no
 
 async function getRandomPhoto() {
-  const url = "https://api.unsplash.com/photos/random/?client_id=yowF489jYmvy_0eVWJW5ClELc7mYi_LNJKLjNN3A2no";
+  const url = "http://localhost:1776/api/getRandomImage";
   try {
     const response = await fetch(url);
     const data = await response.json()
-    const recievedPhoto = data.urls.regular
-
+    const recievedPhoto = data
     const imgDiv = document.querySelector(".back-img")
     imgDiv.style.backgroundImage = `url(${recievedPhoto})`
   } catch (error) {
@@ -23,38 +22,38 @@ async function getRandomPhoto() {
   }
 }
 getRandomPhoto();
-// const quotes = [
-  // {
-    // quote: "You have to dig it to dig it. You Dig?",
-    // author: "Thelonious Monk",
-  // },
+const quotes = [
+  {
+    quote: "You have to dig it to dig it. You Dig?",
+    author: "Thelonious Monk",
+  },
 
-  // {
-    // quote: "There is no life I know to compare with pure imagination.",
-    // author: "Willie Wonka",
-  // },
+  {
+    quote: "There is no life I know to compare with pure imagination.",
+    author: "Willie Wonka",
+  },
 
-  // {
-    // quote: "I'd walk through hell in a gasoline suit to play baseball",
-    // author: "Pete Rose",
-  // },
+  {
+    quote: "I'd walk through hell in a gasoline suit to play baseball",
+    author: "Pete Rose",
+  },
 
-  // {
-    // quote: "He who would learn to fly one day must first learn to stand and walk and run and climb and dance; one cannot fly into flying.",
-    // author: "Friedrich Nietzsche",
-  // },
-// ];
-// function loopThroughQuotes() {
-  // let currentIndex = 0;
-  // setInterval(() => {
-    // if (currentIndex < quotes.length) {
-      // el.quote.innerText = quotes[currentIndex].quote;
-      // el.author.innerText = quotes[currentIndex].author;
-      // currentIndex++;
-    // } else {
-      // currentIndex = 0;
-    // }
-  // }, 4000);
-// }
+  {
+    quote: "He who would learn to fly one day must first learn to stand and walk and run and climb and dance; one cannot fly into flying.",
+    author: "Friedrich Nietzsche",
+  },
+];
+function loopThroughQuotes() {
+  let currentIndex = 0;
+  setInterval(() => {
+    if (currentIndex < quotes.length) {
+      el.quote.innerText = quotes[currentIndex].quote;
+      el.author.innerText = quotes[currentIndex].author;
+      currentIndex++;
+    } else {
+      currentIndex = 0;
+    }
+  }, 4000);
+ }
 
-// setTimeout(loopThroughQuotes, 4000);
+setTimeout(loopThroughQuotes, 4000);
--- server.js
+++ server.js
@@ -5,11 +5,40 @@ const app = express();
 const path = require("path");
 const port = 1776;
 
+require("dotenv").config();
+const cors = require("cors")
+
+app.use(cors());
+
+
 // Serves the front-end content in the public directory
 app.use("", express.static(path.join(__dirname, "./public")));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+async function getRandomPhoto() {
+  const url = `https://api.unsplash.com/photos/random/?client_id=${process.env.CLIENT_ID}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json()
+    const recievedPhoto = data.urls.regular
+
+    return recievedPhoto;
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+app.get("/api/getRandomImage", (request, response) => {
+  getRandomPhoto().then((returnedData) => {
+    response.status(200).json({
+      status: 200,
+      data: returnedData,
+  });
+
+  })
+});
 
 // Serves the whole app
 app.listen(port, () => {
